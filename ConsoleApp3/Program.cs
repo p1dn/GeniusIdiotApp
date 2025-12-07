c599998e6cc4300ef6f55c10d72ed988ee7802da
AlexeySkorikov <skorikov0107@gmail.com>
2025-12-07 04:35:14 +0100

    подзадача 1 переименовал переменную с задачами, добавил счётчик для правильных ответов и вывод вопросов

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -1,10 +1,12 @@
-﻿namespace ConsoleApp3
+﻿using System.Threading.Tasks;
+
+namespace ConsoleApp3
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Dictionary<string, int> qestionsAndAnswers = new Dictionary<string, int>()
+            Dictionary<string, int> tasks = new Dictionary<string, int>()
             {
                 {"Сколько будет два плюс два умноженное на два?", 6},
                 {"Бревно нужно распилить на 10 частей. Сколько распилов нужно сделать?", 9},
@@ -12,6 +14,13 @@
                 {"Укол делают каждые полчаса. Сколько нужно минут, чтобы сделать три укола?", 60},
                 {"Пять свечей горело, две потухли. Сколько свечей осталось?", 2}
             };
+
+            int countOfRightAnswers = 0;
+
+            foreach (var task in tasks)
+            {
+                Console.WriteLine(task.Key);
+            }
         }
     }
 }
