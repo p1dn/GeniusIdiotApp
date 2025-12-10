1a4c4185dde95c00d9971da4164f936dd85f903c
AlexeySkorikov <skorikov0107@gmail.com>
2025-12-10 01:23:43 +0100

    добавил новый класс User

---
+++ ConsoleApp3/User.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp3
+{
+    public class User
+    {
+        public string Name { get; set; } = "Гость";
+        public int СountOfRightAnswers { get; set; } = 0;
+    }
+}
