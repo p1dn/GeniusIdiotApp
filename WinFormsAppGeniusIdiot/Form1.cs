6f0fb754eb2686aeb1c9b5fdacd783a24eb164c1
AlexeySkorikov <skorikov0107@gmail.com>
2025-12-21 19:19:27 +0100

    реализация нажатия кнопки

--- WinFormsAppGeniusIdiot/Form1.cs
+++ WinFormsAppGeniusIdiot/Form1.cs
@@ -1,9 +1,14 @@
 using ConsoleApp3;
+using System.Diagnostics.Metrics;
 
 namespace WinFormsAppGeniusIdiot
 {
     public partial class Form1 : Form
     {
+        private List<Question> questions;
+        private Question currentQuestion;
+        private User user = new User();
+
         public Form1()
         {
             InitializeComponent();
@@ -11,13 +16,30 @@ namespace WinFormsAppGeniusIdiot
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            List<Question> questions = QuestionRepository.GetDefaultQuestions();
-            questionLabel.Text = questions[0].Problem;
+            questions = QuestionRepository.GetDefaultQuestions();
+            user.Name = "Unknown";
+
+            showNextQuestion();
         }
 
         private void nextButton_Click(object sender, EventArgs e)
         {
+            int userAnswer = Convert.ToInt32(userAnswerTextBox.Text);
+            int correctAnswer = currentQuestion.CorrectAnswer;
 
+            if (userAnswer == currentQuestion.CorrectAnswer) user.CountOfRightAnswers++;
+            questions.Remove(currentQuestion);
+
+            showNextQuestion();
+        }
+
+        private void showNextQuestion()
+        {
+            var rnd = new Random();
+            int randomIndex = rnd.Next(0, questions.Count);
+
+            currentQuestion = questions[randomIndex];
+            questionLabel.Text = currentQuestion.Problem;
         }
 
         private void questionNumberLabel_Click(object sender, EventArgs e)
